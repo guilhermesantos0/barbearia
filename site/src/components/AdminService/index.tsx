7251957407c390e8235f067f70fa559758857d94
Guilherme Santos <guilhermesantosdev@outlook.com>
2025-09-09 17:38:39 -0300

    feat: quase nada

--- site/src/components/AdminService/index.tsx
+++ site/src/components/AdminService/index.tsx
@@ -5,6 +5,8 @@ import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 import MustacheIcon from '@assets/icons/mustache.svg?react';
 import { ReactNode } from 'react';
+import { fomratTimeDuration } from '@utils/formatTimeDuration';
+import { formatPrice } from '@utils/formatPrice';
 
 interface AdminServiceProps {
     service: IService;
@@ -13,25 +15,47 @@ interface AdminServiceProps {
 interface ServiceInfosProps {
     icon: ReactNode,
     label: string,
-    value: string
+    value?: string,
+    children?: ReactNode
 }
 
 const AdminService:React.FC<AdminServiceProps> = ({ service }) => {
 
-    const ServiceInfos:React.FC<ServiceInfosProps> = ({ icon, label, value }) => {
+    const ServiceInfos:React.FC<ServiceInfosProps> = ({ icon, label, value, children }) => {
         return (
             <div className={style.ServiceInfo}>
                 <div className={style.ServiceInfoLabelSection}>
                     {icon}
                     <h4 className={style.ServiceInfoLabel}>{label}</h4>
                 </div>
-                <h3 className={style.ServiceInfoValue}>{value}</h3>
+                {
+                    value && (
+                        <h3 className={style.ServiceInfoValue}>{value}</h3>
+                    )
+                }
+                {
+                    children && ( children )
+                }
             </div>
         )
     }
 
     const serviceInfosArray = [
-        
+        {
+            icon: <FontAwesomeIcon icon={['far', 'clock']} />,
+            label: 'Duração',
+            value: fomratTimeDuration(service.duration)
+        },
+        {
+            icon: <FontAwesomeIcon icon='dollar' />,
+            label: 'Preço',
+            value: formatPrice(service.price)
+        },
+        {
+            icon: <FontAwesomeIcon icon='staff' />,
+            label: 'Status',
+            children: <div className={`${style.Status} ${service.active ? style.Active : style.Inactive}`} > {service.active ? 'Ativo' : 'Inativo'} </div>
+        }
     ]
 
     return (
@@ -56,6 +80,13 @@ const AdminService:React.FC<AdminServiceProps> = ({ service }) => {
                     <h2 className={style.Name}>{service.name}</h2>
                     <h4 className={style.Description}>{service.description}</h4>
                 </div>
+            </div>
+            <div className={style.ServiceInfos}>
+                {
+                    serviceInfosArray.map((serviceInfo, idx) => (
+                        <ServiceInfos icon={serviceInfo.icon} label={serviceInfo.label} value={serviceInfo.value || undefined} children={serviceInfo.children || undefined} />
+                    ))
+                }
             </div>
         </div>
     )
